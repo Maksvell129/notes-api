ab970a72f697c59d57ccfba40aa19b9c0c2d530f
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2024-04-01 12:33:46 +0300

    added settings file

---
+++ src/settings.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    database_username: str
+    database_password: str
+    database_hostname: str
+    database_port: str
+    database_name: str
+
+    model_config = SettingsConfigDict(env_file="../.env")
+
+
+settings = Settings()
