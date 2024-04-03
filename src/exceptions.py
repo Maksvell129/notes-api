6ca14b2cfc51277b152da0a180920bd6acd1ecf5
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2024-04-03 10:01:12 +0300

    added exception handler for NotFoundException

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,13 +1,19 @@
-from fastapi import HTTPException
+from fastapi import Request
 from fastapi import status
+from fastapi.responses import JSONResponse
 
 
-class BaseHTTPException(HTTPException):
-    status_code = None
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(status_code=self.status_code, **kwargs)
+class BaseHTTPException(Exception):
+    def __init__(self, detail: str | None = None) -> None:
+        self.detail = detail
 
 
 class NotFoundException(BaseHTTPException):
-    status_code = status.HTTP_404_NOT_FOUND
+    pass
+
+
+async def not_found_exception_handler(request: Request, exc: NotFoundException):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"message": exc.detail},
+    )
--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.api.routers import router
+from src.exceptions import not_found_exception_handler
+from src.exceptions import NotFoundException
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_exception_handler(NotFoundException, not_found_exception_handler)
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
